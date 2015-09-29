4f9f46402b0c8bc54b35fa52c10457fda6394fc8
StianLervik <stian.lervik@outlook.com>
2015-09-29 19:24:18 +0200

    Låg inn spelet
    Har lagt inn spelet.

---
+++ RadøyQuest/Assets/Skript/BussSkript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BussSkript : MonoBehaviour {
+
+	public float svingeFart;
+	public float kjoreFart;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * Time.deltaTime * kjoreFart);
+		transform.RotateAround(transform.position, Vector3.up, 10.0f * Time.deltaTime * svingeFart);
+	}
+}
