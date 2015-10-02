ecfa6006b07ea1fe0c8f441a69e3b6075ee2347d
StianLervik <stian.lervik@outlook.com>
2015-10-02 11:51:06 +0200

    Addar spel

---
+++ RadøyQuest/Assets/RaceCarBehaviour.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaceCarBehaviour : MonoBehaviour {
+
+	public float groundDistance = 1.0f;
+	public float akselerasjon = 100.0f;
+	public float maxSpeed = 2.0f;
+	public float gravity = -10.0f;
+	public float svingeFart = 2.0f;
+	public Animator animator;
+
+	private float currentSpeed;
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody>();
+		if(animator == null){
+			Debug.LogWarning("Ingen animator funnet!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//sjekkDistanse();
+		bevegelse ();
+	}
+
+	private void bevegelse (){
+		Vector3 kraft = Vector3.up * gravity;
+
+		if(Input.GetAxis("Vertical") > 0){
+			if(body.velocity.magnitude < maxSpeed){
+				kraft += (transform.forward * Time.deltaTime * akselerasjon);
+			}
+		}
+		if(Input.GetAxis("Vertical") < 0){
+			if(body.velocity.magnitude < maxSpeed){
+				kraft += (-transform.forward * Time.deltaTime * akselerasjon);
+			}
+		}
+		if(Input.GetAxis("Strobe") > 0){
+			if(body.velocity.magnitude < maxSpeed){
+				kraft += (transform.right * Time.deltaTime * akselerasjon);
+			}
+		}
+		if(Input.GetAxis("Strobe") < 0){
+			if(body.velocity.magnitude < maxSpeed){
+				kraft += (-transform.right * Time.deltaTime * akselerasjon);
+			}
+		}
+		if(Input.GetAxis("Horizontal") > 0){
+			transform.Rotate(transform.up, svingeFart * Time.deltaTime * 100);
+		}
+		if(Input.GetAxis("Horizontal") < 0){
+			transform.Rotate(transform.up, svingeFart * Time.deltaTime * -100);
+		}
+
+
+		body.AddForce(kraft);
+	}
+
+	private void sjekkDistanse(){
+		RaycastHit[] hit;
+		Ray ray = new Ray(transform.position, Vector3.down);
+		hit = Physics.RaycastAll(ray, 5);
+		bool funnet = false;
+		int iterator = 0;
+		while(!funnet && iterator<hit.Length){
+			if(hit[iterator].transform.tag == "Bane"){
+				funnet = true;
+			}else{
+				iterator++;
+			}
+		}
+
+		// Lag og sett ny posisjon
+		if(funnet){
+			Vector3 groundToShip = (hit[iterator].normal.normalized * maxSpeed);
+			Vector3 newTransform = hit[iterator].point + groundToShip;
+			transform.position = newTransform;
+		}
+	}
+}
---
+++ RadøyQuest/Assets/Skript/DagOgNatt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DagOgNatt : MonoBehaviour {
+
+	public float dagFart;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.RotateAround(transform.position, transform.up, 1.0f * Time.deltaTime * dagFart);
+	}
+}
